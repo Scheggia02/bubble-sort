1910fe73c5427f1eed34955136ec0b24936fd5c4
daniele leo <danieleleo16@gmail.com>
2020-01-18 08:49:59 +0100

    Bubble Sort Algorithm

--- algorithm.py
+++ algorithm.py
@@ -2,11 +2,13 @@ from timeit import time
 from random import randrange
 
 #Create a Random List of integers
-lista = [randrange(1000) for _ in range(10)]
+my_list = [randrange(1000) for _ in range(1000)]
 
 # ------ My BubbleSort Algorithm ------
 
 def OwnBubbleSort(list):
+    start_time = time.time() 
+
     n = len(list) - 1
     while n > 0:
         for i in range(n):
@@ -16,8 +18,11 @@ def OwnBubbleSort(list):
                 list[i] = list[i + 1]
                 list[i + 1] = copy
                 # ----->
-        n -= 1
+        n -= 1 #Decrement the range of the Cycle
+
+    stop_time = time.time()
+    print(f"{(stop_time-start_time):.7f}")
 
 
-OwnBubbleSort(lista)
-print(lista)
+OwnBubbleSort(my_list) #Invoke BubbleSort function
+print(my_list)  #Print on console my_list 
